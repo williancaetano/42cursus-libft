31f50c2680aa36224e83c84d22a88bbac98354cd
wcaetano <wcaetano@student.42.rio>
2022-05-21 17:55:56 -0300

    Fixed edge case

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: wcaetano <wcaetano@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/21 13:09:10 by wcaetano          #+#    #+#             */
-/*   Updated: 2022/05/21 17:19:50 by wcaetano         ###   ########.fr       */
+/*   Updated: 2022/05/21 17:52:24 by wcaetano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,11 @@ void	ft_putnbr_fd(int n, int fd)
 	int				i;
 
 	i = 0;
+	if (n == 0)
+	{
+		write(fd, "0", 1);
+		return ;
+	}
 	if (n < 0)
 	{
 		tmp = -n;
