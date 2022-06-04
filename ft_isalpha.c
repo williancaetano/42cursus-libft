8a44bad712e4dbbbfdfdc4271f73441507d44f58
wcaetano <wcaetano@student.42.rio>
2022-06-04 12:11:59 -0300

    Remove unused headers

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,7 +6,7 @@
 /*   By: wcaetano <wcaetano@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 10:45:03 by WCAETANO          #+#    #+#             */
-/*   Updated: 2022/05/21 13:07:44 by wcaetano         ###   ########.fr       */
+/*   Updated: 2022/06/04 12:11:13 by wcaetano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 int	ft_isalpha(int c)
 {
-	if (c > UCHAR_MAX)
-		return (0);
-	return (((unsigned char) c >= 'a' && (unsigned char) c <= 'z')
-		|| ((unsigned char) c >= 'A' && (unsigned char) c <= 'Z'));
+	return ((c >= 'a' && c <= 'z')
+		|| (c >= 'A' && c <= 'Z'));
 }
