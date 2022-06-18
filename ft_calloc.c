04b5507e18087afd007df689a411eab203d7ba98
wcaetano <wcaetano@student.42.rio>
2022-06-18 14:44:59 -0300

    Fixed calloc floating point exception

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: wcaetano <wcaetano@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/14 13:47:35 by wcaetano          #+#    #+#             */
-/*   Updated: 2022/06/04 11:54:44 by wcaetano         ###   ########.fr       */
+/*   Updated: 2022/06/18 14:39:38 by wcaetano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,14 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	void	*ptr;
 	size_t	total_size;
 
+	if (!nmemb || !size)
+		return (ft_calloc(1, 1));
 	total_size = nmemb * size;
 	if ((total_size / nmemb) != size)
 		return (NULL);
 	ptr = malloc(total_size);
 	if (!ptr)
-	{
-		free(ptr);
 		return (NULL);
-	}
 	ft_bzero(ptr, total_size);
 	return (ptr);
 }
